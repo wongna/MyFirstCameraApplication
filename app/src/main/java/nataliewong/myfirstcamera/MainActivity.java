bfc4edab2ebd560bc4084d5441edd0fd4231a824
wongna <natalie.wong@ucalgary.ca>
2017-03-19 19:18:27 -0600

    Added ImageViews and Buttons to the onCreate method in MainActivity.java

--- app/src/main/java/nataliewong/myfirstcamera/MainActivity.java
+++ app/src/main/java/nataliewong/myfirstcamera/MainActivity.java
@@ -2,12 +2,38 @@ package nataliewong.myfirstcamera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView startImage, previewImage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startImage = (ImageView) findViewById(R.id.startImage);
+        previewImage = (ImageView) findViewById(R.id.previewImage);
+
+        Button takeNewPictureButton = (Button) findViewById(R.id.takeNewPictureButton);
+        Button editPictureButton = (Button) findViewById(R.id.editPictureButton);
+
+        takeNewPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //  TODO: Next time, we'll use this to switch Activities!
+            }
+        });
+
+        editPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //  TODO: Next time, we'll use this to edit previewImage!
+            }
+        });
     }
 }
