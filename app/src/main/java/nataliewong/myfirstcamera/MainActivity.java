4cedf616203c044ba5e700f428d440cfe51606e6
wongna <natalie.wong@ucalgary.ca>
2017-03-21 00:56:19 -0600

    Completed the onClick event of the takeNewPictureButton in MenuActivity.java

--- app/src/main/java/nataliewong/myfirstcamera/MainActivity.java
+++ app/src/main/java/nataliewong/myfirstcamera/MainActivity.java
@@ -1,13 +1,18 @@
 package nataliewong.myfirstcamera;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final int MENUACTIVITY_REQUESTCODE = 42;
     private ImageView startImage, previewImage;
 
     @Override
@@ -25,7 +30,10 @@ public class MainActivity extends AppCompatActivity
         takeNewPictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //  TODO: Next time, we'll use this to switch Activities!
+                Toast.makeText(getApplicationContext(), "Launching camera...", Toast.LENGTH_LONG).show();
+
+                Intent switchActivities = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivityForResult(switchActivities, MENUACTIVITY_REQUESTCODE);
             }
         });
 
@@ -35,5 +43,11 @@ public class MainActivity extends AppCompatActivity
                 //  TODO: Next time, we'll use this to edit previewImage!
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent activityData)
+    {
+        //  TODO: Soon, we'll add the behaviour for displaying a captured image!
     }
 }
